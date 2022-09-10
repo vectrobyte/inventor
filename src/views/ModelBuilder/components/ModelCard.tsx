863c805addcd7b620a14b3b28834a39abdc88799
Dilip Bhattarai <vectrobyte@gmail.com>
2022-09-10 20:10:29 +0545

    feat: add dropdown card and push new field on add field event

--- src/views/ModelBuilder/components/ModelCard.tsx
+++ src/views/ModelBuilder/components/ModelCard.tsx
@@ -1,9 +1,11 @@
 import React, { useMemo } from 'react';
 
-import { Field, Model } from '../../../@types';
+import { Field, FieldType, Model } from '../../../@types';
+import { uid } from '../../../common/helpers';
+import Dropdown from '../../../components/Dropdown';
 import Select, { SelectOption } from '../../../components/form/Select';
 import TextField from '../../../components/form/TextField';
-import FieldInput from './FieldInput';
+import FieldInput, { FIELD_OPTIONS } from './FieldInput';
 
 type ModelCardProps = React.HTMLAttributes<HTMLElement> & {
   model: Model;
@@ -19,26 +21,39 @@ const ModelCard: React.FC<ModelCardProps> = ({ model, onUpdate }) => {
         label: field.name,
         value: field.type,
       })),
-    [model.fields]
+    [fields]
   );
 
-  const handleChange = (key: keyof Model, value: string) => {
+  const handleChange = (key: keyof Model, value: any) => {
     onUpdate({
       ...model,
       [key]: value,
     });
   };
 
-  const handleFieldUpdate = (updatedField: Field) => {
-    onUpdate({
-      ...model,
-      fields: fields.map((field) => (updatedField.id === field.id ? updatedField : field)),
-    });
+  const handleAddField = (type: FieldType) => {
+    handleChange('fields', [
+      ...fields,
+      {
+        id: uid(),
+        name: '',
+        type,
+      },
+    ]);
+  };
+
+  const handleUpdateField = (updatedField: Field) => {
+    handleChange(
+      'fields',
+      fields.map((field) => (updatedField.id === field.id ? updatedField : field))
+    );
   };
 
   return (
-    <div className="bg-white rounded-lg border border-gray-200 overflow-hidden shadow-md">
-      <div className="bg-gray-800 py-4 md:px-6 lg:px-8 min-h-[56px] text-white">{model.title}</div>
+    <div className="bg-white rounded-lg border border-gray-200 shadow-md">
+      <div className="bg-gray-800 py-4 md:px-6 lg:px-8 min-h-[56px] rounded-t-lg text-white">
+        {model.title}
+      </div>
 
       <form className="space-y-4 p-4 md:p-6 lg:p-8" action="#">
         <TextField
@@ -57,8 +72,15 @@ const ModelCard: React.FC<ModelCardProps> = ({ model, onUpdate }) => {
         <p>Fields</p>
 
         {fields.map((field, key) => (
-          <FieldInput key={`${field.id}-${key}`} field={field} onUpdate={handleFieldUpdate} />
+          <FieldInput key={`${field.id}-${key}`} field={field} onUpdate={handleUpdateField} />
         ))}
+
+        <Dropdown
+          label="Add Field"
+          options={FIELD_OPTIONS}
+          optionMapper={(option: any) => option.label}
+          onSelect={handleAddField}
+        />
       </form>
     </div>
   );
