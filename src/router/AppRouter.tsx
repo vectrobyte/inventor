e6b4a5ba298b184fa5ee41ffa9101e05dae6f15a
Dilip Bhattarai <vectrobyte@gmail.com>
2022-09-10 13:44:09 +0545

    feat: initialize router with page routes and remove App component files

---
+++ src/router/AppRouter.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from '../views/Home/Home';
+import Model from '../views/Model/Model';
+import ModelBuilder from '../views/ModelBuilder/ModelBuilder';
+
+type AppRouterProps = React.HTMLAttributes<HTMLElement>;
+
+const AppRouter: React.FC<AppRouterProps> = () => {
+
+  return (
+    <Routes>
+      <Route path="/home" element={<Home />} />
+      <Route path="/model/:modelId" element={<Model />} />
+      <Route path="/model-builder" element={<ModelBuilder />} />
+    </Routes>
+  );
+};
+
+export default AppRouter;
