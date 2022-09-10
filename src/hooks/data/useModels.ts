109d72f18ae967a7deadf5e4ed0581666f9ea289
Dilip Bhattarai <vectrobyte@gmail.com>
2022-09-10 14:42:24 +0545

    feat: create useModels hook and define CRUD functions needed for performing model CRUD stored on store

---
+++ src/hooks/data/useModels.ts
@@ -0,0 +1,30 @@
+import { Model } from '../../@types';
+import { STORE_ACTIONS } from '../../context/store';
+import { useStore } from '../useStore';
+
+export function useModels() {
+  const [{ models }, dispatch] = useStore();
+
+  const setModels = (updatedModels: Model[]) => {
+    dispatch({ type: STORE_ACTIONS.setModels, payload: updatedModels });
+  };
+
+  const addModel = (model: Model) => {
+    setModels([...models, model]);
+  };
+
+  const updateModel = (updatedModel: Model) => {
+    setModels(models.map((model) => (updatedModel.id === model.id ? updatedModel : model)));
+  };
+
+  const dropModel = (modelToDelete: Model) => {
+    setModels(models.filter((model) => modelToDelete.id !== model.id));
+  };
+
+  return {
+    models,
+    addModel,
+    updateModel,
+    dropModel,
+  };
+}
