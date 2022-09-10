98743ad4fec32571b552f32f085ba0e38932523f
Dilip Bhattarai <vectrobyte@gmail.com>
2022-09-10 20:24:50 +0545

    feat: extract cross button into a separate component

---
+++ src/components/CrossButton.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+type CrossButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+const CrossButton: React.FC<CrossButtonProps> = ({ onClick }) => {
+  return (
+    <button
+      type="button"
+      className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-full text-sm p-0.5 -mr-2 inline-flex items-center transition-colors"
+      onClick={onClick}
+    >
+      <svg
+        aria-hidden="true"
+        className="w-4 h-4"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fillRule="evenodd"
+          d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+          clipRule="evenodd"
+        />
+      </svg>
+    </button>
+  );
+};
+
+export default CrossButton;
--- src/views/ModelBuilder/components/FieldInput.tsx
+++ src/views/ModelBuilder/components/FieldInput.tsx
@@ -1,6 +1,7 @@
 import React, { ChangeEvent, useMemo } from 'react';
 
 import { Field, FieldType } from '../../../@types';
+import CrossButton from '../../../components/CrossButton';
 import { SelectOption } from '../../../components/form/Select';
 
 type FieldInputProps = {
@@ -76,25 +77,7 @@ const FieldInput: React.FC<FieldInputProps> = ({ field, onUpdate, onDelete }) =>
         </select>
       </div>
 
-      <button
-        type="button"
-        className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-full text-sm p-0.5 -mr-2 inline-flex items-center transition-colors"
-        onClick={onDelete}
-      >
-        <svg
-          aria-hidden="true"
-          className="w-4 h-4"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fillRule="evenodd"
-            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-            clipRule="evenodd"
-          />
-        </svg>
-      </button>
+      <CrossButton onClick={onDelete} />
     </div>
   );
 };
