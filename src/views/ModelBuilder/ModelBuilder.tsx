882289da9134fd3ea8b7ef57341ab4dbc37c7590
Dilip Bhattarai <vectrobyte@gmail.com>
2022-09-10 18:59:29 +0545

    feat: render model cards on builder page

--- src/views/ModelBuilder/ModelBuilder.tsx
+++ src/views/ModelBuilder/ModelBuilder.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 
+import { useModels } from '../../hooks/data/useModels';
+import ModelCard from './components/ModelCard';
+
 type ModelBuilderProps = React.HTMLAttributes<HTMLElement>;
 
 const ModelBuilder: React.FC<ModelBuilderProps> = () => {
+  const { models, updateModel } = useModels();
+
   return (
-    <div className="">
-      <h1>This is model builder page</h1>
+    <div className="model-builder-page w-full">
+      <div className="-mx-4 flex">
+        {models.map((model, key) => (
+          <div key={`${model.id}-${key}`} className="p-4 w-full md:w-1/2 lg:w-1/4">
+            <ModelCard model={model} onUpdate={updateModel} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
