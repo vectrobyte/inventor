f46cf85ab7d0dd9b1072e9cba3a97f24a2f8e847
Dilip Bhattarai <vectrobyte@gmail.com>
2022-09-11 07:36:15 +0545

    refactor: remove dark styling

--- src/components/form/Checkbox.tsx
+++ src/components/form/Checkbox.tsx
@@ -14,7 +14,7 @@ const Checkbox: React.FC<CheckboxProps> = ({ label, value, onChange }) => {
       <input
         type="checkbox"
         checked={value}
-        className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+        className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500"
         onChange={handleChange}
       />
       <span className="ml-2 text-gray-700 text-sm font-medium select-none">{label}</span>
