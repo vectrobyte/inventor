ba942acc4085299e837c3a89a7b6ca5483606c08
Dilip Bhattarai <vectrobyte@gmail.com>
2022-09-11 10:47:03 +0545

    chore: update service worker config

--- src/index.tsx
+++ src/index.tsx
@@ -8,11 +8,9 @@ import ScrollToTop from './components/ScrollToTop';
 import { Providers } from './context';
 import reportWebVitals from './reportWebVitals';
 import AppRouter from './router/AppRouter';
-import registerServiceWorker from './serviceWorker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-
-registerServiceWorker();
 
 root.render(
   <React.StrictMode>
@@ -24,6 +22,11 @@ root.render(
     </HashRouter>
   </React.StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
