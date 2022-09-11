9e4cca2403354a117411ab1a37e6bbb32622e5c8
Dilip Bhattarai <vectrobyte@gmail.com>
2022-09-11 08:04:53 +0545

    refactor: change grid spacing over breakpoints

--- src/components/GridView.tsx
+++ src/components/GridView.tsx
@@ -3,13 +3,13 @@ import React from 'react';
 type GridItemProps = React.HTMLAttributes<HTMLElement>;
 
 export const GridItem: React.FC<GridItemProps> = ({ children }) => {
-  return <div className="p-8 w-full md:w-1/2 lg:w-[450px]">{children}</div>;
+  return <div className="p-4 md:p-6 w-full md:w-1/2 lg:w-[450px]">{children}</div>;
 };
 
 type GridViewProps = React.HTMLAttributes<HTMLElement>;
 
 const GridView: React.FC<GridViewProps> = ({ children }) => {
-  return <div className="md:-mx-8 flex flex-wrap">{children}</div>;
+  return <div className="md:-mx-4 md:p-6 flex flex-wrap">{children}</div>;
 };
 
 export default GridView;
